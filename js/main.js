4679b22d20786d48fbaefde32838efd2744f295a
devomtechnologies <devomtechnologies@gmail.com>
2026-03-23 15:49:39 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
+function verifyPayment() {
+    let userId = document.getElementById("userid").value;
 
+    if (userId === "") {
+        alert("Enter User ID first");
+        return;
+    }
+
+    alert("Payment Verified Successfully!");
+
+    localStorage.setItem(userId, "paid");
+
+    window.location.href = "tms_login.html";
+}
+
+
+function login() {
+    let user = document.getElementById("userid").value;
+    let pass = document.getElementById("password").value;
+
+    if (user === "" || pass === "") {
+        alert("Please fill all fields");
+        return;
+    }
+
+    let paymentStatus = localStorage.getItem(user);
+
+    if (paymentStatus !== "paid") {
+        alert("Please complete payment first!");
+        return;
+    }
+
+    alert("Login Successful!");
+    window.location.href = "dashboard.html";
+}
